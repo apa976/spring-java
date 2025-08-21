9a962d54ea9e53b0574e972cd431905c0c3d30e8
Amit Pandita <amit.pandita@ing.com>
2025-08-21 16:07:27 +0200

    initial commit

---
+++ src/test/java/com/ap976/spring_java/SpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap976.spring_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
