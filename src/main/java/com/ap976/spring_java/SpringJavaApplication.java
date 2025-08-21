82e4936b456e8c85dad5e23fca5e5dcaeb7feda3
Amit Pandita <amit.pandita@ing.com>
2025-08-21 22:15:07 +0200

    Hotfix applied

--- src/main/java/com/ap976/spring_java/SpringJavaApplication.java
+++ src/main/java/com/ap976/spring_java/SpringJavaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringJavaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Application started - Hotfix applied");
 		SpringApplication.run(SpringJavaApplication.class, args);
 	}
 
